1cb74d7c5de9443045510a27076f838741cdb8ed
Riley Bell <rileybell484@gmail.com>
2026-01-28 02:02:27 +1100

    clean up main plugin interface a bit

--- src/main/java/com/example/plugin/interfaces/ModdedServerPlugin.java
+++ src/main/java/com/example/plugin/interfaces/ModdedServerPlugin.java
@@ -12,16 +12,17 @@ import javax.annotation.Nonnull;
 // Simple wrapper around JavaPlugin to make behaviour less annoying...
 public abstract class ModdedServerPlugin extends JavaPlugin {
 
+    public ModdedServerPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+        console.log("Initializing plugin " + this.getName());
+    }
+
     protected HashMap<String, ComponentType<ChunkStore, ? extends Component<ChunkStore>>> registeredBlocks =
         new HashMap<String, ComponentType<ChunkStore, ? extends Component<ChunkStore>>>();
     private static final HytaleLogger.Api console = HytaleLogger.forEnclosingClass().atInfo();
 
     protected void setup() {
         console.log("Setting up plugin " + this.getName());
-    }
-
-    public ModdedServerPlugin(@Nonnull JavaPluginInit init) {
-        super(init);
     }
 
     public <T extends Component<ChunkStore>> void addToRegister(String id, ComponentType<ChunkStore, T> block) {
