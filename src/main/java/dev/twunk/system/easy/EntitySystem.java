8baf3a8c1bba55bff2b1bbdca109a0c527f869ed
Riley Bell <rileybell484@gmail.com>
2026-02-13 11:43:11 +1100

    remove my ticking component code from the entity system

--- src/main/java/dev/twunk/system/easy/EntitySystem.java
+++ src/main/java/dev/twunk/system/easy/EntitySystem.java
@@ -13,19 +13,15 @@ import com.hypixel.hytale.component.dependency.Order;
 import com.hypixel.hytale.component.dependency.SystemDependency;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.RefSystem;
-import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import dev.twunk.component.IRegisteredComponent;
 import dev.twunk.component.ITickingComponent;
 import dev.twunk.plugin.ModPlugin;
-import dev.twunk.utils.BlockUtils;
 import java.util.Set;
 import java.util.function.Supplier;
 import javax.annotation.Nonnull;
 
 public abstract class EntitySystem<T extends ITickingComponent> {
-
-    private static HytaleLogger.Api console = HytaleLogger.forEnclosingClass().atInfo();
 
     @Nonnull
     private final LifetimeSystem lifetimeSystem = new LifetimeSystem();
@@ -123,78 +119,13 @@ public abstract class EntitySystem<T extends ITickingComponent> {
     /**
      * No touchy. just read. overwrite it if you need
      */
-    protected void onTickInternal(
+    protected void tick(
         float dt,
         int index,
         @Nonnull ArchetypeChunk<ChunkStore> archetypeChunk,
         @Nonnull Store<ChunkStore> store,
         @Nonnull CommandBuffer<ChunkStore> commandBuffer
-    ) {
-        // blockEntityRef --(has)-> blockInfo --(is in)-> worldChunk --(is in)-> world
-        // blockInfo has local coords
-        // blockInfo & worldChunk --(implies)-> global coords
-
-        // ref to our block entity (ref to the entity at the current
-        // index that matches the query - imagine the index as an arbtrary
-        // `i` in a for loop. think nothing of it)
-        var blockRef = archetypeChunk.getReferenceTo(index);
-
-        // From my understanding, this seems to be a sort of inherent component
-        // on block entites. You can always seem to get it. This stores its LOCAL
-        // coordinates (sort of)
-        //
-        // It stores the `index` that the block is within its `chunk`. we do some
-        // funky maths to turn that 1 dimensional index (1, 2, 3...) into a 3 dimensional
-        // position [(x: 0, y: 0, z: 0), (x: 1, y: 0, z: 0)... etc]
-        //
-        // note: ^^ above numbers made up, really never checked which order they
-        // index their blocks into the chunk
-        var blockInfo = BlockUtils.getInfo(commandBuffer, blockRef);
-        if (blockInfo == null) {
-            return;
-        }
-
-        // you guessed it, the chunk the block is in. the chunk stores its own coordinates
-        // (as in where the chunk itself is located within the world at large)
-        //
-        // we need this to effectively just add its coordinates to our block
-        // -> block local coords + chunk coords ~= global position
-        var worldChunk = BlockUtils.getWorldChunk(commandBuffer, blockInfo);
-        if (worldChunk == null) {
-            return;
-        }
-
-        // the world our chunk is in
-        // the best way i've found so far to get the world that the entity is in
-        // is to go
-        // blockEntityRef --(has)-> blockInfo --(is in)-> worldChunk --(is in)-> world
-        var world = worldChunk.getWorld();
-        if (world == null) {
-            return;
-        }
-        var coords = BlockUtils.getGlobalCoords(worldChunk, blockInfo);
-
-        // Since our query is based on your component, we KNOW it has to have your
-        // component, so, we just, get it
-        var component = BlockUtils.getComponent(EntitySystem.this.tickingComponentType, commandBuffer, blockRef);
-        try {
-            // and call the tick method you defined on your component, which,
-            // i know is sort of heresy for ECS systems, but, it makes doing
-            // easy things easy. and i'm all for that
-            component.onTick(
-                world,
-                worldChunk,
-                commandBuffer,
-                coords.x,
-                coords.y,
-                coords.z,
-                worldChunk.getBlock(coords)
-            );
-        } catch (Throwable e) {
-            console.log(String.format("ERROR: Failed to tick block at (%d, %d, %d)", coords.x, coords.y, coords.z));
-            return;
-        }
-    }
+    ) {}
 
     /**
      * Tiny sub-system to simply tell our parent system when we added entities
@@ -270,7 +201,7 @@ public abstract class EntitySystem<T extends ITickingComponent> {
             @Nonnull Store<ChunkStore> store,
             @Nonnull CommandBuffer<ChunkStore> commandBuffer
         ) {
-            EntitySystem.this.onTickInternal(dt, index, archetypeChunk, store, commandBuffer);
+            EntitySystem.this.tick(dt, index, archetypeChunk, store, commandBuffer);
         }
 
         @Override
