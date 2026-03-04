2e4732867d786ae2c7bee949d158cbc26d81bce3
Riley Bell <rileybell484@gmail.com>
2026-03-04 20:50:14 +1100

    auto-register-ticking-components add auto register method. also, seems like we've got better non-null and nullable types coming through from the hytale team now (hype)

--- src/main/java/dev/twunk/plugin/ModPlugin.java
+++ src/main/java/dev/twunk/plugin/ModPlugin.java
@@ -3,11 +3,14 @@ package dev.twunk.plugin;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.Component;
 import com.hypixel.hytale.component.ComponentType;
+import com.hypixel.hytale.component.system.ISystem;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
+import dev.twunk.component.IAutoTickingBlockComponent;
 import dev.twunk.component.IRegisteredComponent;
+import dev.twunk.system.TickableBlockComponentSystem;
 import dev.twunk.utils.TwunkLib;
 import javax.annotation.Nonnull;
 
@@ -30,6 +33,14 @@ public abstract class ModPlugin extends JavaPlugin {
     }
 
     /**
+     * Register the given system to the plugin
+     * @param system
+     */
+    public final void registerSystem(@Nonnull final ISystem<ChunkStore> system) {
+        this.getChunkStoreRegistry().registerSystem(system);
+    }
+
+    /**
      * Register the specified component via codec. Does NOT setup
      * system/initialiser.
      * Useful especially for non-ticking components
@@ -38,18 +49,28 @@ public abstract class ModPlugin extends JavaPlugin {
      * instead
      */
     @Nonnull
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public <T extends Component<ChunkStore>> ComponentType<ChunkStore, T> registerComponent(BuilderCodec<T> codec) {
-        var myClass = codec.getInnerClass();
+        Class<T> myClass = codec.getInnerClass();
         var defaultId = myClass.getName();
         console.log("Adding component " + defaultId + " -- from class " + myClass);
         if (defaultId == null) {
             throw new RuntimeException("Failed to get classname while registering component with codec " + codec);
         }
 
-        var component = this.getChunkStoreRegistry().registerComponent(myClass, defaultId, codec);
+        ComponentType<ChunkStore, T> component = this.getChunkStoreRegistry().registerComponent(
+            myClass,
+            defaultId,
+            codec
+        );
 
         // Store our component in the global register
         IRegisteredComponent.registerComponentType(myClass, component);
+
+        if (IAutoTickingBlockComponent.class.isAssignableFrom(myClass)) {
+            // Not sure how to fix this type issue in java, know it should work so i'm really not that worried but yeah...
+            new TickableBlockComponentSystem(component).registerTo(this);
+        }
 
         return component;
     }
