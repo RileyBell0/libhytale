fff0d8096e4d7f94bb141ea5f2e23ddea9c5ac05
Riley Bell <rileybell484@gmail.com>
2026-03-08 12:12:57 +1100

    remove those pesky linter errors

--- src/main/java/dev/twunk/utils/world/TestUtil.java
+++ src/main/java/dev/twunk/utils/world/TestUtil.java
@@ -1,6 +1,7 @@
 package dev.twunk.utils.world;
 
 import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
@@ -14,6 +15,11 @@ import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import javax.annotation.Nonnull;
 
 public final class TestUtil {
+
+    @SuppressWarnings("null")
+    @Nonnull
+    private static final ComponentType<ChunkStore, BlockStateInfo> BLOCK_INFO_COMPONENT =
+        BlockStateInfo.getComponentType();
 
     @Nonnull
     public final WorldProvider worldProvider;
@@ -60,10 +66,27 @@ public final class TestUtil {
         this.world = commandBuffer.getExternalData().getWorld();
         this.worldChunk = worldChunk;
 
-        this.blockChunk = worldChunk.getBlockComponentChunk();
+        var blockChunk = worldChunk.getBlockComponentChunk();
+        if (blockChunk == null) {
+            throw new RuntimeException("ERROR: BlockChunk was null!!!");
+        }
+
+        this.blockChunk = blockChunk;
+
         this.blockRef = blockRef;
-        this.chunkRef = worldChunk.getReference();
-        this.info = blockRef.getStore().getComponent(blockRef, BlockStateInfo.getComponentType());
+        var chunkRef = worldChunk.getReference();
+        if (chunkRef == null) {
+            throw new RuntimeException("ERROR: chunk ref was null!!");
+        }
+
+        this.chunkRef = chunkRef;
+        var info = blockRef.getStore().getComponent(blockRef, BLOCK_INFO_COMPONENT);
+        if (info == null) {
+            throw new RuntimeException("ERROR: info was null");
+        }
+
+        this.info = info;
+
         console.log(this.toString());
     }
 }
