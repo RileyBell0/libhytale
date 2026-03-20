fae40f9dcf2ddacf91b58618cb0a409d10187e76
Riley Bell <rileybell484@gmail.com>
2026-03-20 19:00:09 +1100

    some notes

--- src/main/java/dev/twunk/utils/ItemUtils.java
+++ src/main/java/dev/twunk/utils/ItemUtils.java
@@ -4,11 +4,25 @@ import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
+import com.hypixel.hytale.server.npc.util.InventoryHelper;
 import javax.annotation.Nonnull;
 
-public abstract class ItemUtils {
+public abstract class ItemUtils extends com.hypixel.hytale.server.core.entity.ItemUtils {
+
+    // This is a "stack" of items, i believe all items should be considered a stack, really
+    // hoping that a single item is just a stack of "1" items
+    private static final Class<?> Item = ItemStack.class;
+
+    // This seems to contain all the stuff i already wanted working
+    //
+    // Notably, hytale seems to have really focussed in on entities and general
+    // interactions like THIS and not the backend logic of blocks/interactions in
+    // that regard. This entity code seems way more polished than literally anything
+    // related to chunks and blocks and the world itself
+    private static final Class<?> InvHelper = InventoryHelper.class;
 
     @SuppressWarnings("unused")
     private static final HytaleLogger.Api console = HytaleLogger.forEnclosingClass().atInfo();
