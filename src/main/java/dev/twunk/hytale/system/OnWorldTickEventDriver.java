cfb7fb0b713e178a20e4648b624cea8be311d121
Riley Bell <rileybell484@gmail.com>
2026-04-17 23:03:08 +1000

    git now finally understands i renamed it

--- src/main/java/dev/twunk/hytale/system/OnWorldTickEventDriver.java
+++ src/main/java/dev/twunk/hytale/system/OnWorldTickEventDriver.java
@@ -28,7 +28,7 @@ import javax.annotation.Nullable;
  * @see ArchetypeTickingSystem - I use this to run the subsystem. Only way i currently know
  *                               of for getting a commandBuffer in a global tick
  */
-public class OnUniverseTickEventDriver<ECS_TYPE extends WorldProvider>
+public class OnWorldTickEventDriver<ECS_TYPE extends WorldProvider>
     extends ArchetypeTickingSystem<ECS_TYPE>
     implements ISubSystem<ECS_TYPE>
 {
@@ -45,14 +45,14 @@ public class OnUniverseTickEventDriver<ECS_TYPE extends WorldProvider>
      * Hytale expects a new "class" for each system you register. Thus, to have these composable modules
      * of subsystems, each one must secretly create a new class each and every time you call it
      */
-    public static <ECS_TYPE extends WorldProvider> OnUniverseTickEventDriver<ECS_TYPE> newUninitialised(
+    public static <ECS_TYPE extends WorldProvider> OnWorldTickEventDriver<ECS_TYPE> newUninitialised(
         IOnWorldTick<ECS_TYPE> listener,
         Query<ECS_TYPE> query,
         IRegistry<ECS_TYPE> registry
     ) {
         return ISubSystem.__construct(
             ISubSystem.__dupeClassAndGetConstructor(
-                OnUniverseTickEventDriver.class,
+                OnWorldTickEventDriver.class,
                 IOnWorldTick.class,
                 Query.class,
                 IRegistry.class
@@ -63,7 +63,7 @@ public class OnUniverseTickEventDriver<ECS_TYPE extends WorldProvider>
         );
     }
 
-    protected OnUniverseTickEventDriver(
+    protected OnWorldTickEventDriver(
         IOnWorldTick<ECS_TYPE> listener,
         Query<ECS_TYPE> query,
         IRegistry<ECS_TYPE> registry
