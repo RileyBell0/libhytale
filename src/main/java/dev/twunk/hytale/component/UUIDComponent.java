49bd8bd5d3e54f355ca51dedcc51cea57483ce64
Riley Bell <rileybell484@gmail.com>
2026-04-26 23:12:03 +1000

    yeah its a bit of a nightmare, not sure if any of this works, but ive simplified uuid component to simply just put UUIDs on elements. atm its usefulnes is, questionable for blocks, as yeah, i don't really have a great way to go back to coords from world and all that, but it can give you a unique identifier at least? and thats neat for my specific case right now? really it would be better if it was actually storing block coords or smthn but whatever i can fix that later, need an MVP first

--- src/main/java/dev/twunk/hytale/component/UUIDComponent.java
+++ src/main/java/dev/twunk/hytale/component/UUIDComponent.java
@@ -6,17 +6,19 @@ import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.codec.validation.Validators;
 import com.hypixel.hytale.component.Component;
 import com.hypixel.hytale.server.core.universe.world.WorldProvider;
-import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import com.hypixel.hytale.server.core.util.UUIDUtil;
 import java.util.UUID;
 import javax.annotation.Nonnull;
 import org.bson.UuidRepresentation;
 import org.bson.internal.UuidHelper;
 
-final class Chunk extends UUIDComponent<ChunkStore> {
+public class UUIDComponent<ECS_TYPE extends WorldProvider> implements Component<ECS_TYPE> {
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static final BuilderCodec<Chunk> CODEC = BuilderCodec.builder(Chunk.class, Chunk::new)
+    public static final BuilderCodec<UUIDComponent> CODEC = BuilderCodec.builder(
+        UUIDComponent.class,
+        UUIDComponent::new
+    )
         .append(
             new KeyedCodec("UUID", Codec.UUID_BINARY),
             (o, i) -> o.uuid = i == null ? UUIDUtil.generateVersion3UUID() : i,
@@ -25,55 +27,6 @@ final class Chunk extends UUIDComponent<ChunkStore> {
         .addValidator(Validators.nonNull())
         .add()
         .build();
-
-    public Chunk() {
-        super();
-    }
-
-    // combination of world, chunk, local index
-    public Chunk(UUID coords) {
-        super(coords);
-    }
-
-    // combination of world, chunk, local index
-    public Chunk(UUID worldId, long chunkCoords, int blockCoords) {
-        super(UUIDComponent.uuidFromBlockCoords(worldId, chunkCoords, blockCoords));
-    }
-}
-
-final class Entity extends UUIDComponent<ChunkStore> {
-
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static final BuilderCodec<Chunk> CODEC = BuilderCodec.builder(Chunk.class, Chunk::new)
-        .append(
-            new KeyedCodec("UUID", Codec.UUID_BINARY),
-            (o, i) -> o.uuid = i == null ? UUIDUtil.generateVersion3UUID() : i,
-            o -> o.uuid
-        )
-        .addValidator(Validators.nonNull())
-        .add()
-        .build();
-
-    public Entity() {
-        super();
-    }
-
-    // combination of world, chunk, local index
-    public Entity(UUID uuid) {
-        super(uuid);
-    }
-
-    public static Entity generateVersion3UUID() {
-        return new Entity(UUIDUtil.generateVersion3UUID());
-    }
-
-    @SuppressWarnings("null")
-    public static Entity randomUUID() {
-        return new Entity(UUID.randomUUID());
-    }
-}
-
-public abstract class UUIDComponent<ECS_TYPE extends WorldProvider> implements Component<ECS_TYPE> {
 
     protected UUID uuid;
 
@@ -92,11 +45,12 @@ public abstract class UUIDComponent<ECS_TYPE extends WorldProvider> implements C
         return this;
     }
 
-    // combination of world, chunk, local index
+    // combination of world, chunk, local index, neat idea but am not using it atm
     public static final UUID uuidFromBlockCoords(UUID worldId, long chunkCoords, int blockCoords) {
         return combine(worldId, new UUID(chunkCoords, blockCoords));
     }
 
+    // neat idea but am not using it atm
     public static final UUID combine(UUID a, UUID b) {
         // Source - https://stackoverflow.com/a/5683621
         // Posted by pickypg, modified by community. See post 'Timeline' for change history
